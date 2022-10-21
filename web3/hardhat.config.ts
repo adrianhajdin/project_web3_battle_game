054beb435bc4cc19d2b016851a652a7a11fe59ca
Adrian Hajdin - JS Mastery <contact@jsmastery.pro>
2022-10-21 10:08:40 +0200

    Update hardhat.config.ts

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -8,7 +8,7 @@ dotenv.config();
 //* Why deploy on a subnet: https://docs.avax.network/subnets/when-to-use-subnet-vs-c-chain
 //* How to deploy on a subnet: https://docs.avax.network/subnets/create-a-local-subnet
 //* Transactions on the C-Chain might take 2-10 seconds -> the ones on the subnet will be much faster
-//* On C-Chain we're relaying on the Avax token to confirm transactions -> on the submet we can create our own towen
+//* On C-Chain we're relaying on the Avax token to confirm transactions -> on the subnet we can create our own token
 //* You are in complete control over the network and it's inner workings
 
 export default {
